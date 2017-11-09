b80985b1b996a4c54c9f13252fd61dd7212d146e
Peter <Peter@mBp8qwRwro9Wq.local>
2017-11-09 19:17:25 +0100

    materials added

--- src/app/models/scheduleItem.model.ts
+++ src/app/models/scheduleItem.model.ts
@@ -7,6 +7,9 @@ export interface ScheduleItem {
     classHour:string;
     content:string;
     classContent:string;
+    materials:string;    
+    classMaterials:string;
+    hrefMaterials:string;
     more:string;    
     classMore:string;
     separatorClass:string;
